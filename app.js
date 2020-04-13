37da524f9e5a010aadc8e814495e409030aa9c93
Rommel Rico <me@rommelrico.com>
2020-04-13 05:55:33 -0700

    :sparkles: Basics: Objects

---
+++ app.js
@@ -0,0 +1,5 @@
+var person = {
+    name: "Rommel",
+    age: 32
+};
+console.log("Person: ", person);
---
+++ app.ts
@@ -0,0 +1,6 @@
+const person = {
+  name: "Rommel",
+  age: 32
+};
+
+console.log("Person: ", person);
