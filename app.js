7754691a63ad60e16be9b52c6e1aadd25891b150
Rommel Rico <me@rommelrico.com>
2020-04-13 05:43:52 -0700

    :sparkles: Basics: Using a boolean

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-function add(n1, n2) {
-    console.log(typeof number1);
+function add(n1, n2, showResult) {
+    if (showResult) {
+        console.log("n1 + n2: ", n1 + n2);
+    }
     return n1 + n2;
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
+var printResult = true;
+// Basic Types:
+// - number
+// - string
+// - boolean
+var result = add(number1, number2, printResult);
 console.log("result: ", result);
--- app.ts
+++ app.ts
@@ -1,14 +1,18 @@
-function add(n1: number, n2: number): number {
+function add(n1: number, n2: number, showResult: boolean): number {
+  if (showResult) {
+    console.log("n1 + n2: ", n1 + n2);
+  }
   return n1 + n2;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
 
 // Basic Types:
 // - number
 // - string
 // - boolean
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult);
 console.log("result: ", result);
