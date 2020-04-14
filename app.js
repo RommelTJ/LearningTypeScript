5dc0b568ea2f08c226ee455b192963bfd3dcf86c
Rommel Rico <me@rommelrico.com>
2020-04-14 05:02:05 -0700

    :sparkles: Basics: Combining different types

---
+++ app.js
@@ -0,0 +1,7 @@
+function combine(input1, input2) {
+    return input1 + input2;
+}
+var combinedAges = combine(30, 26);
+console.log("combinedAges: ", combinedAges);
+var combinedNames = combine("Max", "Anna");
+console.log("combinedNames: ", combinedNames);
--- app.ts
+++ app.ts
@@ -1,3 +1,9 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+function combine(input1: number, input2: number) {
+  return input1 + input2;
 }
+
+const combinedAges = combine(30, 26);
+console.log("combinedAges: ", combinedAges);
+
+const combinedNames = combine("Max", "Anna");
+console.log("combinedNames: ", combinedNames);
