631315f1b6d7667bd4102aed0462e48361e1ab0d
Rommel Rico <me@rommelrico.com>
2020-04-20 05:52:40 -0700

    :sparkles: NextGen: Note on var

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 const userName = "Rommel";
 let age = 30;
 age = 29;
+
+// var lets you used in global or function scope.
+var result;
+function add(a: number, b: number) {
+  result = a + b;
+  return result;
+}
+
+console.log(result);
