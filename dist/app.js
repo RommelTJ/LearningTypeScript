99c308c1ffd380203a33fcf284d75c506a50df51
Rommel Rico <me@rommelrico.com>
2020-04-20 07:02:41 -0700

    :sparkles: NextGen: Compiling to ES5

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,42 @@
 "use strict";
-const userName = "Rommel";
-const hobbies = ["Sports", "Cooking"];
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var userName = "Rommel";
+var hobbies = ["Sports", "Cooking"];
 console.log(hobbies[0]);
-const activeHobbies = ["Hiking"];
-activeHobbies.push(...hobbies);
+var activeHobbies = ["Hiking"];
+activeHobbies.push.apply(activeHobbies, hobbies);
 console.log("activeHobbies: ", activeHobbies);
-const person = {
+var person = {
     nickname: "Rommel",
     age: 32
 };
-const copiedPerson = Object.assign({}, person);
+var copiedPerson = __assign({}, person);
 console.log("copiedPerson: ", copiedPerson);
-const add = (...numbers) => {
-    return numbers.reduce((curResult, curValue) => {
+var add = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (curResult, curValue) {
         return curResult + curValue;
     }, 0);
 };
-const addedNumbers = add(5, 10, 2, 3.7);
+var addedNumbers = add(5, 10, 2, 3.7);
 console.log("addedNumbers: ", addedNumbers);
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2);
 console.log("hobby1: ", hobby1);
 console.log("hobby2: ", hobby2);
 console.log("remainingHobbies: ", remainingHobbies);
-const { nickname: username, age } = person;
+var username = person.nickname, age = person.age;
 console.log("username: ", username);
 console.log("age: ", age);
