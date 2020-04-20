b4a465b030a8056c4fd7bcfaf8ad3f788562a129
Rommel Rico <me@rommelrico.com>
2020-04-20 07:03:20 -0700

    :sparkles: NextGen: Compiled js

--- dist/app.js
+++ dist/app.js
@@ -1,42 +1,27 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var userName = "Rommel";
-var hobbies = ["Sports", "Cooking"];
+const userName = "Rommel";
+const hobbies = ["Sports", "Cooking"];
 console.log(hobbies[0]);
-var activeHobbies = ["Hiking"];
-activeHobbies.push.apply(activeHobbies, hobbies);
+const activeHobbies = ["Hiking"];
+activeHobbies.push(...hobbies);
 console.log("activeHobbies: ", activeHobbies);
-var person = {
+const person = {
     nickname: "Rommel",
     age: 32
 };
-var copiedPerson = __assign({}, person);
+const copiedPerson = Object.assign({}, person);
 console.log("copiedPerson: ", copiedPerson);
-var add = function () {
-    var numbers = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        numbers[_i] = arguments[_i];
-    }
-    return numbers.reduce(function (curResult, curValue) {
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
         return curResult + curValue;
     }, 0);
 };
-var addedNumbers = add(5, 10, 2, 3.7);
+const addedNumbers = add(5, 10, 2, 3.7);
 console.log("addedNumbers: ", addedNumbers);
-var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2);
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
 console.log("hobby1: ", hobby1);
 console.log("hobby2: ", hobby2);
 console.log("remainingHobbies: ", remainingHobbies);
-var username = person.nickname, age = person.age;
+const { nickname: username, age } = person;
 console.log("username: ", username);
 console.log("age: ", age);
