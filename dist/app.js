80bd6cba606a66f44f29b0dde57f6995d281b5a6
Rommel Rico <me@rommelrico.com>
2020-04-22 06:59:26 -0700

    :sparkles: Classes: Adding properties and methods to the Department

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,22 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log("Department: ", this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log("employees#: ", this.employees.length);
+        console.log("employees: ", this.employees);
+    }
 }
 const accountingDept = new Department("Accounting");
 accountingDept.describe();
-const accountingCopy = { name: "Test", describe: accountingDept.describe };
-accountingCopy.describe();
+accountingDept.addEmployee("Rommel");
+accountingDept.addEmployee("Liza");
+accountingDept.printEmployeeInformation();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -9,10 +10,20 @@ class Department {
     console.log("Department: ", this.name);
   }
 
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log("employees#: ", this.employees.length);
+    console.log("employees: ", this.employees);
+  }
 }
 
 const accountingDept = new Department("Accounting");
 accountingDept.describe(); // "this" is not a parameter.
 
-const accountingCopy = { name: "Test", describe: accountingDept.describe };
-accountingCopy.describe(); // ok!
+accountingDept.addEmployee("Rommel");
+accountingDept.addEmployee("Liza");
+
+accountingDept.printEmployeeInformation();
