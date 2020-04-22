930a8e60fcdb99fd426956941023686838055d1e
Rommel Rico <me@rommelrico.com>
2020-04-22 07:08:13 -0700

    :sparkles: Classes: readonly properties

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ class Department {
   private employees: string[] = [];
 
   // Using shorthand.
-  constructor(private id: string, public name: string) {
+  constructor(private readonly id: string, public name: string) {
   }
 
   describe(this: Department) { // Using a TypeScript hint
