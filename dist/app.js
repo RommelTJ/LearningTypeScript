43e80046eb838b1c23db180e3859d419e312d0ff
Rommel Rico <me@rommelrico.com>
2020-04-28 06:47:13 -0700

    :fire: Classes: Deleting for setup

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,1 @@
 "use strict";
-let add = (n1, n2) => n1 + n2;
-class Person {
-    constructor(n) {
-        if (n)
-            this.name = n;
-        this.age = 32;
-    }
-    greet(phrase) {
-        if (this.name)
-            console.log(`${phrase} ${this.name}`);
-        else
-            console.log("Hi!");
-    }
-}
-let user1;
-user1 = new Person();
-user1.greet("Hi, there I am");
