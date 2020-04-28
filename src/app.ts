9ebc853da79235939043f688dcddfd1023541254
Rommel Rico <me@rommelrico.com>
2020-04-28 07:11:48 -0700

    :sparkles: AdvancedTypes: Discriminated Unions

--- src/app.ts
+++ src/app.ts
@@ -75,3 +75,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+// Discriminated Union Pattern
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+// Discriminated Union
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving with speed: ", speed);
+}
+
+moveAnimal({type: "bird", flyingSpeed: 10});
+moveAnimal({type: "horse", runningSpeed: 6});
