f2c690e4e04f784ff47e34e427d174776d49babb
Rommel Rico <me@rommelrico.com>
2020-04-30 07:13:24 -0700

    :sparkles: Generics: Built-in Generics, Promise

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
 const names = ["Rommel", "Liza"];
+const promise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("This is done!");
+    }, 2000);
+});
+promise.then(data => {
+    data.split(' ');
+});
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 // Generics
 // const names: Array<string> = ["Rommel", "Liza"];
 const names: string[] = ["Rommel", "Liza"]; // equivalent
+
+const promise: Promise<string> = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("This is done!")
+  }, 2000);
+});
+
+promise.then(data => {
+  data.split(' '); // we can do this because TypeScript knows it will yield a string
+});
