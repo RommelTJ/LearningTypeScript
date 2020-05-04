815a876678bd9fc821d0bb05ba766c34cdaa920d
Rommel Rico <me@rommelrico.com>
2020-05-04 06:50:04 -0700

    :sparkles: Decorators: Building more useful decorators

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,16 @@ function Logger(logString: string) {
   }
 }
 
-@Logger("Logging Person")
+function WithTemplate(template: string, hookId: string) {
+  return function(_: Function) {
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  }
+}
+
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
