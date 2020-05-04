724029e908ce5b7b0de98f0658e8e8f21889f1b5
Rommel Rico <me@rommelrico.com>
2020-05-04 07:03:10 -0700

    :sparkles: Decorators: Diving into Property Decorators
    Executes when the property is defined.

--- dist/app.js
+++ dist/app.js
@@ -34,3 +34,26 @@ Person = __decorate([
 ], Person);
 const pers = new Person();
 console.log("pers: ", pers);
+function Log(target, propertyName) {
+    console.log("Property Decorator: ", target, propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("Invalid price: Should be positive!");
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
