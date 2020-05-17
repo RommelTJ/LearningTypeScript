a139994b270e5b326ac2d5b60b74a47645793d38
Rommel Rico <me@rommelrico.com>
2020-05-17 07:24:13 -0700

    :sparkles: DnD: Component base class

--- dist/app.js
+++ dist/app.js
@@ -74,6 +74,21 @@ function validate(validatableInput) {
     }
     return isValid;
 }
+class Component {
+    constructor(templateId, hostElementId, insertAtStart, newElementId) {
+        this.templateElement = document.getElementById(templateId);
+        this.hostElement = document.getElementById(hostElementId);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        if (newElementId) {
+            this.element.id = `${newElementId}-projects`;
+        }
+        this.attach(insertAtStart);
+    }
+    attach(insertAtStart) {
+        this.hostElement.insertAdjacentElement(insertAtStart ? 'afterbegin' : 'beforeend', this.element);
+    }
+}
 class ProjectList {
     constructor(type) {
         this.type = type;
--- src/app.ts
+++ src/app.ts
@@ -90,6 +90,34 @@ function validate(validatableInput: Validatable) {
   return isValid;
 }
 
+// Component Base Class
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string) {
+    this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = `${newElementId}-projects`;
+    }
+
+    this.attach(insertAtStart);
+  }
+
+  private attach(insertAtStart: boolean) {
+    this.hostElement.insertAdjacentElement(insertAtStart ? 'afterbegin' : 'beforeend', this.element);
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
+
+}
+
 // ProjectList Class
 class ProjectList {
   templateElement: HTMLTemplateElement;
