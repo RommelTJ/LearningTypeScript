96f27e974a5ce3101616b62941b9bf0af34a64c3
Rommel Rico <me@rommelrico.com>
2020-05-29 07:21:51 -0700

    :sparkles: Libraries: Product Model

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import _ from 'lodash';
+import Product from "./product.model";
 
-import {ProjectList} from "./components/project-list";
-import {ProjectInput} from "./components/project-input";
+const p1 = new Product('A Book', 12.99);
+console.log(p1.getInformation());
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
-
-console.log(_.shuffle([1, 2, 3]));
+// import _ from 'lodash';
+// import {ProjectList} from "./components/project-list";
+// import {ProjectInput} from "./components/project-input";
+//
+// new ProjectInput();
+// new ProjectList('active');
+// new ProjectList('finished');
+//
+// console.log(_.shuffle([1, 2, 3]));
