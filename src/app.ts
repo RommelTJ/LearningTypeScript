ba00a62bc96531ebf7a91fbdf7f496633d364e05
Rommel Rico <me@rommelrico.com>
2020-05-29 07:13:33 -0700

    :sparkles: Libraries: Using lodash shuffle

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import _ from 'lodash';
 import {ProjectList} from "./components/project-list";
 import {ProjectInput} from "./components/project-input";
 
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
+
+console.log(_.shuffle([1, 2, 3]));
