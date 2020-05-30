604bfb71f18ed65ca05351bc2fca475e14493eb5
Rommel Rico <me@rommelrico.com>
2020-05-30 15:58:21 -0700

    :sparkles: Libraries: No Types-Needed class transformer

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import "reflect-metadata";
+import { plainToClass } from 'class-transformer';
 import Product from "./product.model";
 
 const products = [{title: "A Carpet", price: 29.99}, {title: "A Book", price: 10.99}];
 
 // const p1 = new Product('A Book', 12.99);
 
-const loadedProducts = products.map(p => {
-  return new Product(p.title, p.price);
-})
+// const loadedProducts = products.map(p => {
+//   return new Product(p.title, p.price);
+// })
 
+const loadedProducts = plainToClass(Product, products);
 for (const p of loadedProducts) {
   console.log(p.getInformation());
 }
