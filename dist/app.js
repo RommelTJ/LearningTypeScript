7736f4874b4b6772f900b67ef80dfed5fbba744f
Rommel Rico <me@rommelrico.com>
2020-05-31 08:28:00 -0700

    :sparkles: SharePlace: Setting up a Google API key

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,8 @@
 "use strict";
 const form = document.querySelector("form");
 const addressInput = document.getElementById("address");
+const GOOGLE_API_KEY = "<REDACTED>";
+const req = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY";
 function searchAddressHandler(event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
+const GOOGLE_API_KEY = "<REDACTED>";
+const req = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY";
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
