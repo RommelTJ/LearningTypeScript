a4729b7818d35c376e6bee00d543fcde034fc469
Rommel Rico <me@rommelrico.com>
2020-05-31 08:21:08 -0700

    :sparkles: SharePlace: Getting User Input

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
+const form = document.querySelector("form");
+const addressInput = document.getElementById("address");
+function searchAddressHandler(event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    console.log("enteredAddress: ", enteredAddress);
+}
+form.addEventListener("submit", searchAddressHandler);
 //# sourceMappingURL=app.js.map
