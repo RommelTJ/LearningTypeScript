9386dc0b70c89c1b79b5593f7a2fefd5ae488a85
Rommel Rico <me@rommelrico.com>
2020-06-01 06:09:05 -0700

    :sparkles: SharePlace: Using Axios to fetch coordinates for an entered address

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "<REDACTED>";
-const req = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY";
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-  console.log("enteredAddress: ", enteredAddress);
+  const encodedAddress = encodeURI(enteredAddress);
 
   // send this to Google's API.
+  axios
+    .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
 
 form.addEventListener("submit", searchAddressHandler);
